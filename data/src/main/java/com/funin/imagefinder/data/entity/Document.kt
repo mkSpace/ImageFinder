65a20847fdc0e7bbf8b15ee17fc878af306179b5
Jaemin.Park <once29@naver.com>
2024-05-01 20:56:26 +0900

    Add Document Entity

---
+++ data/src/main/java/com/funin/imagefinder/data/entity/Document.kt
@@ -0,0 +1,44 @@
+package com.funin.imagefinder.domain.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.LocalDateTime
+
+@Entity(tableName = "documents")
+data class Document(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "document_id")
+    val id: Long = 0L,
+
+    @ColumnInfo(name = "collection")
+    val collection: String,
+
+    @Embedded(prefix = "image_")
+    val image: Image,
+
+    @ColumnInfo(name = "display_site_name")
+    val displaySiteName: String,
+
+    @ColumnInfo(name = "doc_url")
+    val docUrl: String,
+
+    @ColumnInfo(name = "date_time")
+    val dateTime: LocalDateTime
+) {
+
+    data class Image(
+        @ColumnInfo(name = "thumbnail_url")
+        val thumbnailUrl: String,
+
+        @ColumnInfo(name = "image_url")
+        val imageUrl: String,
+
+        @ColumnInfo(name = "width")
+        val width: Int,
+
+        @ColumnInfo(name = "height")
+        val height: Int,
+    )
+}
